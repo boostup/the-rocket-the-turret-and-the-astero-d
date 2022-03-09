7604dd03c1e1ccf50d866c91de3753acc083c463
boostup <boostup@gmail.com>
2022-03-09 04:03:47 +0100

    Draggable drags from center of the object to drag + debug layer aligned to it

--- src/models/Draggable.js
+++ src/models/Draggable.js
@@ -19,8 +19,9 @@ promote(DraggableContainer, "Container");
 
 DraggableContainer_prototype.setupDrag = function () {
   this.on("pressmove", function (evt) {
-    this.x = evt.stageX - evt.target.x;
-    this.y = evt.stageY - evt.target.y;
+    const bounds = this.objToDrag.getBounds();
+    this.x = evt.stageX - bounds.width / 2;
+    this.y = evt.stageY - bounds.height / 2;
 
     // Dragging works without this, but it is so much smooter with the following instruction
     this.stage.update();
@@ -35,17 +36,11 @@ export default DraggableContainer;
 DraggableContainer_prototype.setDebugLayer = function (show = false) {
   if (show) {
     this.debugLayer = new Shape();
-    const pad = 0;
-    const bounds = this.objToDrag.getBounds();
 
+    const bounds = this.objToDrag.getBounds();
     this.debugLayer.graphics
       .beginFill("#ABCD")
-      .drawRect(
-        pad + bounds.x,
-        pad + bounds.y,
-        bounds.width + pad,
-        bounds.height + pad
-      );
+      .drawRect(0, 0, bounds.width, bounds.height);
 
     this.addChild(this.debugLayer);
   }
